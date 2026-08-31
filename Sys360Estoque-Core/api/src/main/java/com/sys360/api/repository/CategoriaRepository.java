535feb3f02f550b335a1af1cbbac86ed440caf2a
Gabriel Levi Lima Rodrigues <115875368+Biellima2811@users.noreply.github.com>
2026-08-30 21:34:19 -0300

    Delete Sys360Estoque-Core directory

--- Sys360Estoque-Core/api/src/main/java/com/sys360/api/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sys360.api.repository;
-
-import com.sys360.api.modelo.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, UUID> {
-}
